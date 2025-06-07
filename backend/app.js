ec2d098107c1e9704fe86331e64cb9bac1b4fb35
Olumide Oluwasegun Bola <sekooniolumide@yahoo.com>
2025-06-07 10:49:07 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 const cors = require('cors');
 
 const corsOptions ={
-    origin:'http://localhost:5173', 
+    origin:'https://pluto-ecommerce-store-backend.onrender.com', 
     credentials:true,            
     optionSuccessStatus:200
 }
